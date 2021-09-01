b7d11d6a013ce64604801bc59f55e0b6f3f5faef
caiohscruz <caiohscruz@hotmail.com>
2021-09-01 00:20:47 -0300

    Adiciona funcionalidade de POST

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
+// Setando o express
 const express = require("express")
 const app     = express()
+
+// Componente necessário para capturar valores do formulário
+const bodyParser = require("body-parser")
 
 // Dizendo para o express que o EJS deve ser usado como View Engine
 app.set('view engine', 'ejs')
 // Para que o express reconheça imagens e CSS - public é o nome do diretorio
 app.use(express.static('public'))
 
+//
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
 // Rotas
 
+// Rota para a home
 app.get("/", (req, res) => {
     res.render("index", { page : "home"} )
 })
+
+// Rota para a página de perguntas
 app.get("/asking", (req, res) => {
     res.render("asking", { page : "asking"})
 })
 
-app.post("/save-question", (req, res) => {
-    res.send("Formulário recebido!")
+// Rota para a página de pergunta salva
+app.post("/asking", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send(`Formulário recebido! ${titulo} ${descricao} "`)
 })
 
 app.listen(8081, ()=>{console.log("Testing...")})
