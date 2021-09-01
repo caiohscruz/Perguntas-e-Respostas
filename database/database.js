3b14d1479ab705d353ca444487e7eef2af839528
caiohscruz <caiohscruz@hotmail.com>
2021-09-01 18:17:21 -0300

    Inclui variaveis de ambiente

--- database/database.js
+++ database/database.js
@@ -1,20 +1,15 @@
 // Permite interagir com diferentes bancos de dados
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize(
-    "guiaperguntas", 
-    "root", 
-    "root", {
-    host: "localhost",
-    dialect: "mysql"
-})
-/* Incluir mais para frente as variáveis de ambiente
+// Setando variáveis de ambiente com dotenv
+require('dotenv/config');
+
 const connection = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USERNAME, 
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_SGBD
+    dialect: process.env.DB_DIALECT
 })
-*/
+
 module.exports = connection
