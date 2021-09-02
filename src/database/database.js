de417da950620ce6ae017589946b64774b7cf3a0
caiohscruz <caiohscruz@hotmail.com>
2021-09-02 11:35:50 -0300

    Prepara para deploy no heroku

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,8 @@ const Sequelize = require("sequelize")
 // Setando variáveis de ambiente com dotenv
 require('dotenv/config');
 
+// passando parametros separadamente
+/*
 const connection = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USERNAME, 
@@ -11,5 +13,8 @@ const connection = new Sequelize(
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
 })
+*/
+// passando string de conexao
+const connection = new Sequelize(process.env.DB_CONECCTION_URI)
 
 module.exports = connection
